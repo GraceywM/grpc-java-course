edfd8adca3bdd97832365f9f8228720d4b7a67b7
GraceywM <48034030+GraceywM@users.noreply.github.com>
2021-09-30 22:07:07 -0700

    implement Unary API client

--- src/main/java/com/github/grace/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/grace/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,10 @@
 package com.github.grace.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -14,10 +18,27 @@ public class GreetingClient {
 
         System.out.println("Creating sub");
 
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
         // create an async client
         //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        //created a greetservice
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        //create a protocol buffer message
+        Greeting greeting =Greeting.newBuilder()
+                .setFirstName("Pikachu")
+                .setLastName("Pika")
+                .build();
+
+        //do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+        //Call the RPC and get back a GreetResponse(protocol buffers)
+       GreetResponse greetResponse = greetClient.greet(greetRequest);
+        System.out.println(greetResponse.getResult());
 
         System.out.println("Shutting Down Channel");
         channel.shutdown();
